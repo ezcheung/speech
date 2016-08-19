ae56112fbf4303ec094f448a26f93906e130a0ec
elliotccheung@gmail.com <elliotccheung@gmail.com>
2016-08-18 19:35:38 -0500

    Added main js, implemented changing function

---
+++ Speech/main.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+
+// React package for constructing components (and all non-DOM related actions)
+import React from 'react';
+import mainView from './views/mainView.js';
+
+ReactDOM.render(<mainView />, document.getElementById('app'));
--- Speech/views/mainView.js
+++ Speech/views/mainView.js
@@ -4,7 +4,8 @@ export default class mainView extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      input: ''
+      input: '',
+      vowels: ['a', 'e', 'i', 'o', 'u', 'oo', 'y', 'ou', 'oi', 'ai', 'ao', 'ei', 'oy', 'ay', 'uy']
     }
   }
 
@@ -16,12 +17,22 @@ export default class mainView extends React.Component{
     var component = this;
     return(
       <div className='.main'>
+        <label>
+          Type in here
+        </label>
         <input
           className='.input'
           defaultValue=component.state.input
           onChange= {e=>component.setState({input: e.currentTarget.value})}
         </input>
+        <div>
+          {fn(component.state.input)}
+        </div>
       </div>
     )
+  }
+
+  fn(str){
+    return str.replace(/[aeiouAEIOU]/g, Math.floor(Math.random() * this.state.vowels.length));
   }
 }
